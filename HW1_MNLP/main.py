57287724d6e3b69db88b6675d882a602aae34b02
Valerio <carioti.1983063@studenti.uniroma1.it>
2025-04-15 21:37:20 +0200

    chiamate asincrone

--- HW1_MNLP/main.py
+++ HW1_MNLP/main.py
@@ -4,7 +4,7 @@ Starting point for the script.
 Imports: dataset, graph
 """
 
-from pandas import DataFrame
+import pandas as pd
 
 from modules import dataset, graph
 
@@ -15,15 +15,13 @@ def main() -> None:
     """
 
     # Prepare the training set
-    train_set: DataFrame = dataset.prepare_dataset('train')
-
-    # da scrivere meglio nel prepare dataset
-    train_set = train_set.select_dtypes(include=['number'])
-    train_set.drop(columns=['num_sitelinks'], inplace=True)
+    train_set: pd.DataFrame = dataset.prepare_dataset('train')
+    train_x: pd.DataFrame = train_set.drop(columns=['label']).sample(frac=1, random_state=42)
+    train_y: pd.Series = train_set['label']
 
     for mode, treshold in (('iou', 0.5), ('correlation', 0.45), ('filtered correlation', 0.4)):
         # Calculate the similarity graph
-        graph.get_similarity_graph(train_set, similarity_threshold=treshold, mode=mode, save_fig=True)
+        graph.get_similarity_graph(train_x, similarity_threshold=treshold, mode=mode, save_fig=True)
 
     # Prepare the validation set
     dataset.prepare_dataset('valid')
--- HW1_MNLP/modules/dataset.py
+++ HW1_MNLP/modules/dataset.py
@@ -7,48 +7,80 @@ Useful functions:
 Imports: paths, utils
 """
 
+import aiohttp
+import asyncio
 from collections import Counter
 from collections.abc import Iterable, Sequence
 from typing import Any, Literal
 
 import pandas as pd
 from pathlib import Path
-from tqdm import tqdm
-import requests
+from tqdm.asyncio import tqdm as async_tqdm
 
 from modules import paths, utils
 
 
-def _get_sitelinks(entity_ids: Sequence[str], batch_size: int = 50) -> dict[str, Any]:
+async def _get_sitelinks(entity_ids: Sequence[str], batch_size: int = 50, concurrent_requests: int = 0) -> dict[str, Any]:
     """
-    Use the Wikidata API to fetch sitelinks for a set of entity IDs.
+    Use the Wikidata API to fetch sitelinks for a set of entity IDs asynchronously.
 
     Args:
         entity_ids: sequence of entity IDs to fetch sitelinks for.
         batch_size: number of IDs to fetch in each API call.
+        concurrent_requests: number of concurrent requests to make. If <= 0, no limit will be applied.
     """
 
     sitelinks: dict[str, Any] = {}
 
-    # Fetch sitelinks in batches
-    for i in tqdm(range(0, len(entity_ids), batch_size), desc="Fetching batches"):
-        batch_ids: Sequence[str] = entity_ids[i:i + batch_size]
+    async def fetch_batch(batch_ids: Sequence[str],
+                          session: aiohttp.ClientSession,
+                          semaphore: asyncio.Semaphore|None = None
+                          ) -> None:
+        """
+        Fetch a batch of sitelinks asynchronously and update the sitelinks dictionary.
 
-        # API request
-        url: str = 'https://www.wikidata.org/w/api.php'
-        params: dict[str, str] = {
-            'action': 'wbgetentities',
-            'ids': '|'.join(batch_ids),
-            'format': 'json',
-            'props': 'sitelinks',
-        }
-        response: requests.Response = requests.get(url, params=params)
-        response.raise_for_status()
+        Args:
+            batch_ids: sequence of entity IDs to fetch.
+            session: aiohttp session.
+            semaphore: semaphore to limit the number of concurrent requests. If None, no limit will be applied.
+        """
 
-        # Parse the response
-        entities: dict[str, Any] = response.json().get('entities', {})
-        for entity_id, entity_data in entities.items():
-            sitelinks[entity_id] = entity_data.get('sitelinks', {})
+        async def make_request() -> None:
+            """
+            Make the API request and parse the response.
+            """
+
+            # API request to get the sitelinks
+            url: str = 'https://www.wikidata.org/w/api.php'
+            params: dict[str, str] = {
+                'action': 'wbgetentities',
+                'ids': '|'.join(batch_ids),
+                'format': 'json',
+                'props': 'sitelinks',
+            }
+
+            # Parse the response
+            async with session.get(url, params=params) as response:
+                response.raise_for_status()
+                entities: dict[str, Any] = (await response.json()).get('entities', {})
+                for entity_id, entity_data in entities.items():
+                    sitelinks[entity_id] = entity_data.get('sitelinks', {})
+        
+        # Limit the number of concurrent requests if specified
+        if semaphore:
+            async with semaphore:
+                await make_request()
+        else:
+            await make_request()
+
+    # Fetch the sitelinks in batches
+    async with aiohttp.ClientSession() as session:
+        # Limit the number of concurrent requests if specified
+        semaphore: asyncio.Semaphore|None = asyncio.Semaphore(concurrent_requests) if concurrent_requests > 0 else None
+        await async_tqdm.gather(
+            *(fetch_batch(entity_ids[i:i + batch_size], session, semaphore) for i in range(0, len(entity_ids), batch_size)),
+            desc="Fetching sitelinks"
+        )
 
     return sitelinks
 
@@ -80,7 +112,8 @@ class _CommonPages:
         # take the top pages
         top_pages_tuples: list[tuple[str, int]] = page_counts.most_common(max_pages if max_pages > 0 else None)
         cls.top_pages = tuple(page for page, _ in top_pages_tuples)
-        print(f"Top {max_pages} pages: {cls.top_pages}")
+        if max_pages > 0:
+            print(f"Top {max_pages} pages: {cls.top_pages}")
     
 
     @classmethod
@@ -117,59 +150,94 @@ def _group_by_page(sitelinks: dict[str, dict[str, dict[str, Any]]], pages: Itera
     return page_to_title_to_id
 
 
-def _get_common_page_lenght(sitelinks: dict[str, dict[str, dict[str, Any]]],
-                           find_common_pages: bool = False,
-                           max_pages: int = 0,
-                           batch_size: int = 50
-                           ) -> dict[str, dict[str, int]]:
+async def _get_common_page_lenght(sitelinks: dict[str, dict[str, dict[str, Any]]],
+                                  find_common_pages: bool = False,
+                                  max_pages: int = 0,
+                                  batch_size: int = 50,
+                                  concurrent_requests: int = 50
+                                  ) -> dict[str, dict[str, int]]:
     """
-    Get for each id the number of characters in the most common pages.
+    Get for each id the number of characters in the most common pages asynchronously.
 
     Args:
         sitelinks: dictionary of sitelinks.
         find_common_pages: whether to find the most common pages. Set to True to find the most common pages, otherwise the previously set pages will be used.
         max_pages: maximim number of pages to consider. If <= 0, all pages will be considered.
         batch_size: number of IDs to fetch in each API call.
+        concurrent_requests: number of concurrent requests to make. If <= 0, no limit will be applied.
     """
 
+    # Find the most common pages in the sitelinks
     if find_common_pages:
-        # find the most common pages in the sitelinks
         _CommonPages.set(sitelinks, max_pages)
     top_pages: tuple[str, ...] = _CommonPages.get()
 
-    # group the sitelinks by pages
+    # Group the sitelinks by pages
     page_to_title_to_id: dict[str, dict[str, str]] = _group_by_page(sitelinks, top_pages)
 
-    # map the pages names to their URLs
+    # Map the pages names to their URLs
     site_map: dict[str, str] = utils.PageHandler.get_site_to_url()
 
-    # make the requests in batches
+    # Make the requests in batches
     results: dict[str, dict[str, int]] = {}
-    for page, titles_to_ids in page_to_title_to_id.items():
-        titles: tuple[str, ...] = tuple(titles_to_ids.keys())
-        for i in tqdm(range(0, len(titles_to_ids), batch_size), desc=f"Fetching {page} pages"):
-            titles_batch: tuple[str, ...] = titles[i:i + batch_size]
+    async def fetch_batch(page: str,
+                          titles_batch: tuple[str, ...],
+                          session: aiohttp.ClientSession,
+                          semaphore: asyncio.Semaphore|None = None
+                          ) -> None:
+        """
+        Fetch a batch of page lengths asynchronously and update the results dictionary.
 
-            # API request
+        Args:
+            page: page to fetch.
+            titles_batch: batch of titles to fetch.
+            session: aiohttp session.
+            semaphore: semaphore to limit the number of concurrent requests. If None, no limit will be applied.
+        """
+
+        async def make_request() -> None:
+            """
+            Make the API request and parse the response.
+            """
+
+            # Make the API request
             url: str = f'{site_map[page]}/w/api.php'
-            params: dict[str, str|bool] = {
+            params: dict[str, str | bool] = {
                 'action': 'query',
                 'prop': 'info',
                 'titles': '|'.join(titles_batch),
                 'format': 'json'
             }
-            response: requests.Response = requests.get(url, params=params)
-            response.raise_for_status()
 
             # Parse the response
-            parsed: dict[str, Any] = response.json()
-            pages: dict[str, dict[str, Any]] = parsed.get('query', {}).get('pages', {})
-            
-            for data in pages.values():
-                id: str = titles_to_ids[data['title']]
-                if id not in results:
-                    results[id] = {}
-                results[id][page] = data.get('length', 0)
+            async with session.get(url, params=params) as response:
+                response.raise_for_status()
+                parsed: dict[str, Any] = await response.json()
+                pages: dict[str, dict[str, Any]] = parsed.get('query', {}).get('pages', {})
+
+                for data in pages.values():
+                    id: str = titles_to_ids[data['title']]
+                    if id not in results:
+                        results[id] = {}
+                    results[id][page] = data.get('length', 0)
+
+        # Limit the number of concurrent requests if specified
+        if semaphore:
+            async with semaphore:
+                await make_request()
+        else:
+            await make_request()
+
+    # Fetch the page lengths in batches
+    async with aiohttp.ClientSession() as session:
+        # Limit the number of concurrent requests if specified
+        semaphore: asyncio.Semaphore|None = asyncio.Semaphore(concurrent_requests) if concurrent_requests > 0 else None
+        for page, titles_to_ids in page_to_title_to_id.items():
+            titles: tuple[str, ...] = tuple(titles_to_ids.keys())
+            await async_tqdm.gather(
+                *(fetch_batch(page, titles[i:i + batch_size], session, semaphore) for i in range(0, len(titles), batch_size)),
+                desc=f"Fetching {page} pages"
+            )
 
     return results
 
@@ -192,18 +260,17 @@ def prepare_dataset(split: Literal['train', 'valid']) -> pd.DataFrame:
     df['id'] = df['item'].map(utils.extract_id)
 
     # get the sitelinks for each id
-    sitelinks: dict[str, Any] = _get_sitelinks(df['id'].tolist())
-
-    # add the number of sitelinks to the DataFrame
-    df['num_sitelinks'] = df['id'].map(lambda x: len(sitelinks.get(x, {})))
+    sitelinks: dict[str, Any] = asyncio.run(_get_sitelinks(df['id'].tolist()))
 
     # add the sitelinks lengths to the DataFrame
     find_common_pages: bool = split == 'train' # find common pages only during training
-    common_page_lenght: dict[str, dict[str, int]] = _get_common_page_lenght(sitelinks, find_common_pages = find_common_pages, max_pages = 20)
+    common_page_lenght: dict[str, dict[str, int]] = asyncio.run(_get_common_page_lenght(sitelinks, find_common_pages = find_common_pages, max_pages = 20))
     common_page_lenght_df: pd.DataFrame = pd.DataFrame.from_dict(common_page_lenght, orient='index').fillna(0).astype(int)
-    df = df.merge(common_page_lenght_df, left_on='id', right_index=True, how='left')
+    df.set_index('id', inplace=True)
+    updated_df: pd.DataFrame = common_page_lenght_df.merge(df[['label']], left_index=True, right_index=True, how='left')
     
     # Save the updated dataset to a new file
-    df.to_csv(output_file, index=False)
+    updated_df.to_csv(output_file, index=False)
     print(f"Updated dataset saved to {output_file}")
-    return df
+
+    return updated_df
